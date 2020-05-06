ca3be4b5d6bae041140972d2cddc10396c1006e1
Nitesh Kumar Shaw <niteshshaw2011@gmail.com>
2020-05-06 21:48:04 +0530

    More demo tests

--- MicroLib.DesignPatternsTests/RepositoryPattern/Repository_Functanality.cs
+++ MicroLib.DesignPatternsTests/RepositoryPattern/Repository_Functanality.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace MicroLib.DesignPatternsTests.RepositoryPattern
 {
@@ -26,6 +27,13 @@ namespace MicroLib.DesignPatternsTests.RepositoryPattern
             repo = new Repository<MocProfileInfo, int>(ctx);
         }
 
+        [TestCleanup]
+        public void Cleanup()
+        {
+            ctx = null;
+            repo = null;
+        }
+
         [TestMethod]
         public void Test_AddFunctanality()
         {
@@ -35,6 +43,33 @@ namespace MicroLib.DesignPatternsTests.RepositoryPattern
             var list = repo.GetAll();
 
             Assert.AreEqual(2, list.ToList().Count);
+        }
+
+        [TestMethod]
+        public async Task Test_AddFunctanalityAsync()
+        {
+            await repo.AddAsync(new MocProfileInfo() { Id = 1, FullName = "Tim Waang", Email = "tim.waang@domain.com" });
+            await repo.AddAsync(new MocProfileInfo() { Id = 2, FullName = "Jhon Doe", Email = "doe.jhon@domain.com" });
+            await ctx.SaveChangesAsync();
+            var list = await repo.GetAllAsync();
+
+            Assert.AreEqual(2, list.ToList().Count);
+        }
+
+        [TestMethod]
+        public void Test_DelAndFindFunctanality()
+        {
+            repo.AddAsync(new MocProfileInfo() { Id = 1, FullName = "Tim Waang", Email = "tim.waang@domain.com" }).Wait();
+            repo.AddAsync(new MocProfileInfo() { Id = 2, FullName = "Jhon Doe", Email = "doe.jhon@domain.com" }).Wait();
+            ctx.SaveChangesAsync().Wait();
+
+            var dels = repo.Find(e => e.FullName == "Jhon Doe");
+            repo.Remove(dels);
+
+            var list = repo.GetAllAsync().Result;
+            ctx.SaveChangesAsync().Wait();
+
+            Assert.AreEqual(1, list.ToList().Count);
         }
     }
 }
